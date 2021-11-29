e13fa40112a5d1ffcf7359044f4afe80fe73b438
naojulius <naojulius.mg@gmail.com>
2021-11-30 00:38:52 +0300

    fixed services files

--- source/@api_core/adapters/database-adapter.service.ts
+++ source/@api_core/adapters/database-adapter.service.ts
@@ -1,5 +1,8 @@
 import { Schema, model, connect } from "mongoose";
 export async function connectDatabase(): Promise<void> {
-  //"mongodb+srv://naojulius:naojulius@cluster0.pvbsa.mongodb.net/SaynaApi?retryWrites=true&w=majority"
-  await connect("mongodb://127.0.0.1:27017", { dbName: "SaynaDb" });
+  //await connect("mongodb://127.0.0.1:27017", { dbName: "SaynaDb" });
+  await connect(
+    "mongodb+srv://naojulius:naojulius@cluster0.pvbsa.mongodb.net/SaynaApi?retryWrites=true&w=majority",
+    { dbName: "SaynaDb" }
+  );
 }
